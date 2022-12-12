3105f581e492ad21e86785ff8db0de6846e6ce7c
Gwen Guérin <gwenael.guerin@protonmail.com>
2022-12-12 17:43:18 +0100

    gwen 2nd

--- reducers/user.js
+++ reducers/user.js
@@ -1,8 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
 const initialState = {
-  value: {firstname: null, lastname: null, username: null, email: null, password: null, token: null},
+  value: {
+    firstname: null,
+    lastname: null,
+    username: null,
+    email: null,
+    password: null,
+    token: null,
+  },
 };
 
 export const userSlice = createSlice({
@@ -10,11 +16,24 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     login: (state, action) => {
-      const { firstname, lastname, username, email, password, token } = action.payload;
-    //   console.log(action.payload);
+      // const { firstname, lastname, username, email, password, token } = action.payload;
+      state.value.firstname = action.payload.firstname;
+      state.value.lastname = action.payload.lasname;
+      state.value.username = action.payload.username;
+      state.value.email = action.payload.email;
+      state.value.password = action.payload.password;
+      state.value.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.value.firstname = null;
+      state.value.lastname = null;
+      state.value.username = null;
+      state.value.email = null;
+      state.value.password = null;
+      state.value.token = null;
     },
   },
 });
 
-export const { login } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 export default userSlice.reducer;
